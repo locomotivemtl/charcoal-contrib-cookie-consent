50cfd0cc78f67605a195f4e8d7e73ca6d7dfcd90
Joel Alphonso <info@joelalphonso.com>
2023-12-11 16:18:41 -0500

    feat(cookie-consent): add ConsentService.php

---
+++ src/Charcoal/CookieConsent/ConsentService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Charcoal\CookieConsent;
+
+use Charcoal\Config\ConfigInterface;
+use Charcoal\CookieConsent\Model;
+use Charcoal\CookieConsent\Transformer;
+
+/**
+ * Cookie Consent Service.
+ *
+ * Handle code generation for Cookie Consent functionalities.
+ */
+class ConsentService
+{
+    private ConfigInterface $config;
+    private Model\Consent $consent;
+    private Transformer\Consent $transformer;
+
+    public function __construct(
+        ConfigInterface     $config,
+        Model\Consent       $consent,
+        Transformer\Consent $transformer
+    ) {
+        $this->config = $config;
+        $this->consent = $consent;
+        $this->transformer = $transformer;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPluginSettingsAttr(): string
+    {
+        return json_encode($this->getPluginSettings(), (JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+    }
+
+    /**
+     * @return array
+     */
+    public function getPluginSettings(): array
+    {
+        return array_replace(
+            (array)$this->config,
+            $this->getConsentData()
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getConsentData(): array
+    {
+        return $this->transformer->transform($this->consent->load(1));
+    }
+}
