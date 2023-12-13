26150eab6a88442347786b1597e5695bb128716e
Joel Alphonso <info@joelalphonso.com>
2023-12-13 17:42:27 -0500

    refactor(cookie-consent): move model-structure parsing in transformers to it's own trait

---
+++ src/Charcoal/CookieConsent/Transformer/StructureAwareTrait.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Charcoal\CookieConsent\Transformer;
+
+use Charcoal\Model\ModelInterface;
+use Charcoal\Property\StructureProperty;
+use InvalidArgumentException;
+use Pimple\Container;
+
+trait StructureAwareTrait
+{
+    protected Container $transformers;
+
+    protected function parseStructure(
+        ModelInterface $model,
+        string $propertyIdent,
+        string $transformerIdent = null
+    ): array {
+        $structure = $this->getStructure($model, $propertyIdent);
+        if (!$structure) {
+            return [];
+        }
+        return $this->transformers[$transformerIdent ?? $propertyIdent]->transform($structure);
+    }
+
+    protected function getStructure(ModelInterface $model, string $propertyIdent): ?ModelInterface
+    {
+        if (!$model->hasProperty($propertyIdent)) {
+            throw new InvalidArgumentException(
+                sprintf('Property ident %s does not exist for model %s', $propertyIdent, get_class($model))
+            );
+        }
+
+        if (!$model->p($propertyIdent) instanceof StructureProperty) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Model property from ident %s must be a StructureProperty, %s received',
+                    $propertyIdent,
+                    get_class($model->p($propertyIdent))
+                )
+            );
+        }
+
+        if (!$model[$propertyIdent]) {
+            return null;
+        }
+
+        $pModel = $model->p($propertyIdent)->toModel();
+        $modelData = $model[$propertyIdent];
+        return $pModel->setData($modelData);
+    }
+}
